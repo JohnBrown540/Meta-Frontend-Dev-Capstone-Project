1658955f087b1f0ba41f8450af7735a8905ea45f
JohnBrown540 <brownyj74@gmail.com>
2024-09-30 13:38:45 +0000

    I completed the footer here

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 
 import Main from './components/Main';
 import Menu from './components/Menu';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Nav/>
       <Main/>
       <Menu/>
+      <Footer/>
       
     </div>
   );
