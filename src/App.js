0ba3ea9748ca3d6fb990c0e598f6ab1439af4b80
JohnBrown540 <brownyj74@gmail.com>
2024-09-30 12:08:54 +0000

    I developed the functionality of the booking page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
-import Header from './components/Header';
+
+import Main from './components/Main';
 
 function App() {
   return (
     <div>
       <Nav/>
-      <Header/>
+      <Main/>
+      
     </div>
   );
 }
